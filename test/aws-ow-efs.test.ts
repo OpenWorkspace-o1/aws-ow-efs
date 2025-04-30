e75c66a255785c882a78f9e6b2bd982be5204af4
Trung Dinh <xamarindevelopervietnam@outlook.com>
2025-04-30 13:58:41 +0700

    Init commit

---
+++ test/aws-ow-efs.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsOwEfs from '../lib/aws-ow-efs-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-ow-efs-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsOwEfs.AwsOwEfsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
